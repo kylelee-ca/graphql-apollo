de667127af427a8a33111c8d3823d6228657f6f0
Kyle Lee <kyleleeca@gmail.com>
2022-08-23 23:56:12 -0400

    Add queries for supplies & equipments

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const database = require("./database");
 const { ApolloServer, gql } = require("apollo-server");
+
 const typeDefs = gql`
   type Query {
     teams: [Team]
+    equipments: [Equipment]
+    supplies: [Supply]
+    team(id: Int): Team
   }
   type Team {
     id: Int
@@ -12,11 +16,35 @@ const typeDefs = gql`
     mascot: String
     cleaning_duty: String
     project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: Int
   }
 `;
+//
 const resolvers = {
   Query: {
-    teams: () => database.teams,
+    teams: () =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter((supply) => {
+          return supply.team === team.id;
+        });
+        return team;
+      }),
+    team: (parent, args, context, info) =>
+      database.teams.filter((team) => {
+        return team.id === args.id;
+      })[0],
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
   },
 };
 const server = new ApolloServer({ typeDefs, resolvers });
